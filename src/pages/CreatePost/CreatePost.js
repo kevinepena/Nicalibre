e6a070ed3f3fcc2978cc3a1e9c2edf26ec12ce6a
erikamats <erikamats@hotmail.com>
2018-07-20 17:52:37 -0500

    now first commit

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 // import { withRouter } from "react-router-dom";
-import API from "../../utils/API";
-
 
 class CreatePost extends Component {
   state = {
